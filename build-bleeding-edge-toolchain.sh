6784a888d0f6c16fa005834b9e0b7bde36915802
Freddie Chopin <freddie.chopin@gmail.com>
2017-01-17 08:58:10 +0100

    Remove download step of zlib from zlib.net
    zlib is downloaded from sourceforge, the download step from zlib.net
    was left by mistake.

--- build-bleeding-edge-toolchain.sh
+++ build-bleeding-edge-toolchain.sh
@@ -612,7 +612,6 @@ fi
 if [ "${enableWin64}" = "y" ]; then
 	download ${pythonArchiveWin64} https://www.python.org/ftp/python/${pythonVersion}/${pythonArchiveWin64}
 fi
-download ${zlibArchive} http://zlib.net/${zlibArchive}
 download ${zlibArchive} https://sourceforge.net/projects/libpng/files/zlib/${zlibVersion}/${zlibArchive}
 cd ${top}
 
