1737b5b24f82e6c09e16b29767dc0bf5f8708973
Freddie Chopin <freddie.chopin@gmail.com>
2017-08-14 15:01:07 +0200

    Update newlib to version 2.5.0.20170623
    Currently it is not possible to use the most recent snapshot due to
    build failure.

--- build-bleeding-edge-toolchain.sh
+++ build-bleeding-edge-toolchain.sh
@@ -21,7 +21,7 @@ islVersion="0.18"
 libiconvVersion="1.15"
 mpcVersion="1.0.3"
 mpfrVersion="3.1.5"
-newlibVersion="2.5.0.20170421"
+newlibVersion="2.5.0.20170623"
 pythonVersion="2.7.13"
 zlibVersion="1.2.11"
 
