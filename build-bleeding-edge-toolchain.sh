2afdf876a2443f913078e847efb3e57025f91148
Peter Andersson <pelleplutt1976@gmail.com>
2019-10-18 10:08:48 +0200

    added gdb tui and libncurses

--- build-bleeding-edge-toolchain.sh
+++ build-bleeding-edge-toolchain.sh
@@ -16,6 +16,7 @@ gccVersion="master"
 binutilsVersion="master"
 expatVersion="2.2.7"
 gmpVersion="6.1.2"
+ncursesVersion="6.1"
 islVersion="0.21"
 libiconvVersion="1.16"
 mpcVersion="1.1.0"
@@ -41,6 +42,8 @@ expat="expat-${expatVersion}"
 expatArchive="${expat}.tar.bz2"
 gmp="gmp-${gmpVersion}"
 gmpArchive="${gmp}.tar.xz"
+ncurses="ncurses-${ncursesVersion}"
+ncursesArchive="ncurses-${ncursesVersion}.tar.gz"
 isl="isl-${islVersion}"
 islArchive="${isl}.tar.xz"
 libiconv="libiconv-${libiconvVersion}"
@@ -165,6 +168,44 @@ buildZlib() {
 		if [ "${keepBuildFolders}" = "n" ]; then
 			echo "${bold}---------- ${bannerPrefix}${zlib} remove build folder${normal}"
 			rm -rf ${buildFolder}/${zlib}
+		fi
+	fi
+	)
+}
+
+buildNcurses() {
+	(
+	local buildFolder="${1}"
+	local bannerPrefix="${2}"
+	local configureOptions="${3}"
+	local tagFileBase="${top}/${buildFolder}/ncurses"
+	echo "${bold}********** ${bannerPrefix}${ncurses}${normal}"
+	if [ ! -f "${tagFileBase}_built" ]; then
+		if [ -d "${buildFolder}/${ncurses}" ]; then
+			rm -rf "${buildFolder}/${ncurses}"
+		fi
+		mkdir -p ${buildFolder}/${ncurses}
+		cd ${buildFolder}/${ncurses}
+		export CPPFLAGS="${BASE_CPPFLAGS-} ${CPPFLAGS-}"
+		export LDFLAGS="${BASE_LDFLAGS-} ${LDFLAGS-}"
+		echo "${bold}---------- ${bannerPrefix}${ncurses} configure${normal}"
+		eval "${top}/${sources}/${ncurses}/configure \
+			${quietConfigureOptions} \
+			${configureOptions} \
+			--prefix=${top}/${buildFolder}/${prerequisites}/${ncurses} \
+			--enable-cxx \
+			--disable-stripping \
+			--disable-shared \
+			--disable-nls"
+		echo "${bold}---------- ${bannerPrefix}${ncurses} make${normal}"
+		make -j${nproc}
+		echo "${bold}---------- ${bannerPrefix}${ncurses} make install${normal}"
+		make install
+		touch "${tagFileBase}_built"
+		cd ${top}
+		if [ "${keepBuildFolders}" = "n" ]; then
+			echo "${bold}---------- ${bannerPrefix}${ncurses} remove build folder${normal}"
+			rm -rf ${buildFolder}/${ncurses}
 		fi
 	fi
 	)
@@ -370,8 +411,8 @@ buildBinutils() {
 		fi
 		mkdir -p ${buildFolder}/${binutils}
 		cd ${buildFolder}/${binutils}
-		export CPPFLAGS="-I${top}/${buildFolder}/${prerequisites}/${zlib}/include ${BASE_CPPFLAGS-} -march=haswell ${CPPFLAGS-}"
-		export LDFLAGS="-L${top}/${buildFolder}/${prerequisites}/${zlib}/lib ${BASE_LDFLAGS-} ${LDFLAGS-}"
+		export CPPFLAGS="-I${top}/${buildFolder}/${prerequisites}/${zlib}/include -I${top}/${buildFolder}/${prerequisites}/${ncurses}/include ${BASE_CPPFLAGS-} -march=haswell ${CPPFLAGS-}"
+		export LDFLAGS="-L${top}/${buildFolder}/${prerequisites}/${zlib}/lib -L${top}/${buildFolder}/${prerequisites}/${ncurses}/lib ${BASE_LDFLAGS-} ${LDFLAGS-}"
 		echo "${bold}---------- ${bannerPrefix}${binutils} configure${normal}"
 		pkgversionBinutils=`git describe --all --long --always`
 		eval "${top}/${sources}/${binutils}/configure \
@@ -391,6 +432,7 @@ buildBinutils() {
 			--with-system-gdbinit=${top}/${installFolder}/${target}/lib/gdbinit \
 			--with-system-zlib \
 			--with-expat=yes \
+			--enable-tui \
 			--with-libexpat-prefix=${top}/${buildFolder}/${prerequisites}/${expat} \
 			--with-mpfr=yes \
 			--with-libmpfr-prefix=${top}/${buildFolder}/${prerequisites}/${mpfr} \
@@ -694,6 +736,7 @@ else
 	find ${sources} -mindepth 1 -maxdepth 1 -type f \
 		! -name "${expatArchive}" \
 		! -name "${gmpArchive}" \
+		! -name "${ncursesArchive}" \
 		! -name "${islArchive}" \
 		! -name "${libiconvArchive}" \
 		! -name "${mpcArchive}" \
@@ -732,6 +775,7 @@ checkout() {
 download ${expatArchive} https://github.com/libexpat/libexpat/releases/download/$(echo "R_${expatVersion}" | sed 's/\./_/g')/${expatArchive}
 checkout ${gccVersion} git://gcc.gnu.org/git/gcc.git ${gcc}
 checkout ${binutilsVersion} git://sourceware.org/git/binutils-gdb.git ${binutils}
+download ${ncursesArchive} http://ftp.gnu.org/pub/gnu/ncurses/${ncursesArchive}
 download ${gmpArchive} http://ftp.gnu.org/gnu/gmp/${gmpArchive}
 download ${islArchive} http://isl.gforge.inria.fr/${islArchive}
 if [ "${enableWin32}" = "y" ] || [ "${enableWin64}" = "y" ]; then
@@ -758,6 +802,7 @@ extract() {
 		touch "${1}_extracted"
 	fi
 }
+extract ${ncursesArchive}
 extract ${expatArchive}
 extract ${gmpArchive}
 extract ${islArchive}
@@ -783,6 +828,8 @@ cd ${top}
 hostTriplet=$(${sources}/${newlib}/config.guess)
 
 buildZlib ${buildNative} "" "" ""
+
+buildNcurses ${buildNative} "" "--build=${hostTriplet} --host=${hostTriplet}"
 
 buildGmp ${buildNative} "" "--build=${hostTriplet} --host=${hostTriplet}"
 
@@ -955,6 +1002,11 @@ buildMingw() {
 		${buildFolder} \
 		${bannerPrefix} \
 		"--build=${hostTriplet} --host=${triplet}"
+
+	buildNcurses \
+		${buildFolder} \
+		${bannerPrefix} \
+		"--build=${hostTriplet} --host=${triplet} --enable-term-driver --enable-sp-funcs"
 
 	cat > ${buildFolder}/python.sh <<- EOF
 	#!/bin/sh
