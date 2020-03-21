c4b85a2baed2b69b8bda4aa92066a54d7d9d59aa
Oliver Galvin <odg@riseup.net>
2020-03-21 17:55:35 +0100

    Use HTTPS where possible.

--- build-bleeding-edge-toolchain.sh
+++ build-bleeding-edge-toolchain.sh
@@ -7,7 +7,7 @@
 # author: Copyright (C) 2016-2020 Freddie Chopin http://www.freddiechopin.info http://www.distortec.com
 #
 # This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
-# distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+# distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
 #
 
 set -e
@@ -752,22 +752,22 @@ download() {
 		touch "${1}_downloaded"
 	fi
 }
-download "${binutilsArchive}" "http://ftp.gnu.org/gnu/binutils/${binutilsArchive}"
+download "${binutilsArchive}" "https://ftp.gnu.org/gnu/binutils/${binutilsArchive}"
 download "${expatArchive}" "https://github.com/libexpat/libexpat/releases/download/$(echo "R_${expatVersion}" | sed 's/\./_/g')/${expatArchive}"
 if [ "${gccVersion#*-}" = "${gccVersion}" ]; then
-	download "${gccArchive}" "http://ftp.gnu.org/gnu/gcc/${gcc}/${gccArchive}"
+	download "${gccArchive}" "https://ftp.gnu.org/gnu/gcc/${gcc}/${gccArchive}"
 else
 	download "${gccArchive}" "https://gcc.gnu.org/pub/gcc/snapshots/${gccVersion}/${gccArchive}"
 fi
-download "${gdbArchive}" "http://ftp.gnu.org/gnu/gdb/${gdbArchive}"
-download "${gmpArchive}" "http://ftp.gnu.org/gnu/gmp/${gmpArchive}"
+download "${gdbArchive}" "https://ftp.gnu.org/gnu/gdb/${gdbArchive}"
+download "${gmpArchive}" "https://ftp.gnu.org/gnu/gmp/${gmpArchive}"
 download "${islArchive}" "http://isl.gforge.inria.fr/${islArchive}"
 if [ "${enableWin32}" = "y" ] || [ "${enableWin64}" = "y" ]; then
-	download "${libiconvArchive}" "http://ftp.gnu.org/pub/gnu/libiconv/${libiconvArchive}"
+	download "${libiconvArchive}" "https://ftp.gnu.org/pub/gnu/libiconv/${libiconvArchive}"
 fi
-download "${mpcArchive}" "http://ftp.gnu.org/gnu/mpc/${mpcArchive}"
-download "${mpfrArchive}" "http://ftp.gnu.org/gnu/mpfr/${mpfrArchive}"
-download "${newlibArchive}" "http://sourceware.org/pub/newlib/${newlibArchive}"
+download "${mpcArchive}" "https://ftp.gnu.org/gnu/mpc/${mpcArchive}"
+download "${mpfrArchive}" "https://ftp.gnu.org/gnu/mpfr/${mpfrArchive}"
+download "${newlibArchive}" "https://sourceware.org/pub/newlib/${newlibArchive}"
 if [ "${enableWin32}" = "y" ]; then
 	download "${pythonArchiveWin32}" "https://www.python.org/ftp/python/${pythonVersion}/${pythonArchiveWin32}"
 fi
