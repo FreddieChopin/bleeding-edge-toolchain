5ff9783f7a45fff04df6849cc957d15057f44ef9
Freddie Chopin <freddie.chopin@gmail.com>
2020-03-22 10:48:10 +0100

    Do a "force-overwrite" during a copy early in buildMingw()
    Without `-f` flag the copy fails to overwrite some existing read-only
    files.

--- build-bleeding-edge-toolchain.sh
+++ build-bleeding-edge-toolchain.sh
@@ -926,15 +926,15 @@ buildMingw() {
 	export WINDRES="${triplet}-windres"
 
 	mkdir -p "${installFolder}/${target}"
-	cp -R "${installNative}/${target}/include" "${installFolder}/${target}/include"
-	cp -R "${installNative}/${target}/lib" "${installFolder}/${target}/lib"
+	cp -Rf "${installNative}/${target}/include" "${installFolder}/${target}"
+	cp -Rf "${installNative}/${target}/lib" "${installFolder}/${target}"
 	mkdir -p "${installFolder}/lib"
-	cp -R "${installNative}/lib/gcc" "${installFolder}/lib/gcc"
+	cp -Rf "${installNative}/lib/gcc" "${installFolder}/lib"
 	mkdir -p "${installFolder}/share"
 	if [ "${buildDocumentation}" = "y" ]; then
-		cp -R "${installNative}/share/doc" "${installFolder}/share/doc"
+		cp -Rf "${installNative}/share/doc" "${installFolder}/share"
 	fi
-	cp -R "${installNative}"/share/gcc-* "${installFolder}/share/"
+	cp -Rf "${installNative}"/share/gcc-* "${installFolder}/share"
 
 	(
 		messageA "${bannerPrefix}${libiconv}"
